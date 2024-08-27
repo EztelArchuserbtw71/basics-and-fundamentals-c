aa52b1c324ebff6e47257ee9b30bc5b4a17ae165
@Eztel <142735711+EztelArchuserbtw71@users.noreply.github.com>
2024-08-27 10:42:54 +0800

    Baroja_Ezekiel_C2B_Basics-And-Fundamentals.cpp
    it happen to that vs code code been use for loops and array inn Data structures and algorithm class and forgot to remove it

--- Baroja_Ezekiel_C2B_Basics-And-Fundamentals.cpp
+++ Baroja_Ezekiel_C2B_Basics-And-Fundamentals.cpp
@@ -1,58 +1,51 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-
-void eztelSortDesc(int array[], int size) 
+void eztelSortDesc(int array[], int size)
 {
-    bool swapped;
-    for (int i=0;i<size-1;++i) 
-    {
-      swapped=false;
-      for(int j=0; j<size-i-1; ++j)
-    {
-     if(array[j]<array[j+1]) 
-        {
-            int temp=array[j];
-            array[j]=array[j + 1];
-            array[j + 1]=temp;
-            swapped=true;
-        }
-        }
-        if(!swapped)break;
-    }
+bool swapped;
+for (int i=0;i<size-1;++i)
+{
+swapped=false;
+for(int j=0; j<size-i-1; ++j)
+{
+if(array[j]<array[j+1])
+{
+int temp=array[j];
+array[j]=array[j + 1];
+array[j + 1]=temp;
+swapped=true;
 }
-
+}
+if(!swapped)break;
+}
+}
 int main()
 {
-    int array_1[10], array_2[10], array_fnl[20] = {};
-    
-    cout<<"Input Elements For Array One\n";
-    for(int i= 0;i<10;++i)
-    {
-        cout<<"arrOne["<< i <<"]: ";
-        cin>>array_1[i];
-    }
-    cout<<"Input Elements For Array Two\n";
-    for(int i = 0; i < 10; ++i)
-    {
-       cout<<"array_2["<<i<<"]: ";
-       cin>>array_2[i];
-    }
-    cout<<"Combined Array\n";
-    for(int i = 0; i < 20; ++i)
-    {
-     int tarrayInd=i % 10;
-        
-      array_fnl[i] = i <= 9 ? array_1[tarrayInd] : array_2[tarrayInd];
-      cout<<"array_fnl["<<i<< "]: "<<array_fnl[i]<<"\n";
-    }
-    
-    eztelSortDesc(array_fnl, 20);
-    
-     cout<<"Sorted Array in Descending Order\n";
-    for(int i=0;i<20;++i)
-    {
-     cout <<"array_fnl["<<i<<"]: "<<array_fnl[i]<<"\n";
-    }
-    
-    return 0;
+int array_1[10], array_2[10], array_fnl[20] = {};
+cout<<"Input Elements For Array One\n";
+for(int i= 0;i<10;++i)
+{
+cout<<"arrOne["<< i <<"]: ";
+cin>>array_1[i];
+}
+cout<<"Input Elements For Array Two\n";
+for(int i = 0; i < 10; ++i)
+{
+cout<<"array_2["<<i<<"]: ";
+cin>>array_2[i];
+}
+cout<<"Combined Array\n";
+for(int i = 0; i < 20; ++i)
+{
+int tarrayInd=i % 10;
+array_fnl[i] = i <= 9 ? array_1[tarrayInd] : array_2[tarrayInd];
+cout<<"array_fnl["<<i<< "]: "<<array_fnl[i]<<"\n";
+}
+eztelSortDesc(array_fnl, 20);
+cout<<"Sorted Array in Descending Order\n";
+for(int i=0;i<20;++i)
+{
+cout <<"array_fnl["<<i<<"]: "<<array_fnl[i]<<"\n";
+}
+return 0;
 }
